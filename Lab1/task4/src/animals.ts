d3d48366a397a30112a6a0a937930304c58c0bf1
Arthur-tubus <molofiy350@gmail.com>
2026-09-20 12:11:26 +0300

    Add laboratory work /  Add task 4
    define Animal interface with optional properties and implement classes

---
+++ Lab1/task4/src/animals.ts
@@ -0,0 +1,66 @@
+interface Animal{
+    name: string;
+    age?: number;
+
+    //Пересування
+    run?(): void;
+    fly?(): void;
+    swim?(): void;
+
+    // звуки
+    makeSound(): void;
+}
+
+// ----------------
+class Cat implements Animal {
+    constructor(public name: string, public age: number) {}
+    
+    run(): void {
+        console.log(`${this.name} бігає на чотирьох лапах.`);
+    }
+    
+    makeSound(): void {
+        console.log(`${this.name} каже: Мяу!`);
+    }
+}
+
+const myCat = new Cat("Барсик", 3);
+myCat.makeSound();
+myCat.run();
+
+// ----------------
+class Bird implements Animal {
+    constructor(public name: string, public age: number, public wingspan?: number) {}
+    fly(): void {
+        console.log(`${this.name} летить у небі.`);
+    }
+    
+    run(): void {
+        console.log(`${this.name} ходить на двох лапках.`);
+    }
+    
+    makeSound(): void {
+        console.log(`${this.name} співає: Чирик!`);
+    }
+}
+
+const myBird = new Bird("Воробей", 25);
+myBird.makeSound();
+myBird.fly();
+
+// ----------------
+class Fish implements Animal{
+    constructor(public name: string) {}
+
+    swim(): void {
+        console.log(`${this.name} пливе у воді.`);
+    }
+
+    makeSound(): void {
+        console.log(`${this.name} не видає гучних звуків (буль-буль).`);
+    }
+}
+
+const myFish = new Fish("Немо");
+myFish.makeSound();
+myFish.swim();
